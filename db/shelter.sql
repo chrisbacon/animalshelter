38d3356becafc869fba6f84eb22abd8ffc4fe1be
chrisbacon <bacon.chris@gmail.com>
2016-11-25 10:46:51 +0000

    shelter.sql now creates tables

---
+++ db/shelter.sql
@@ -0,0 +1,16 @@
+DROP TABLE pets;
+DROP TABLE customers;
+
+CREATE TABLE customers (
+    id SERIAL8 primary key,
+    name VARCHAR(255)
+);
+
+CREATE TABLE pets (
+    id SERIAL8 primary key,
+    name VARCHAR(255),
+    species VARCHAR(255),
+    entry DATE,
+    exit DATE,
+    owner_id INT8 references customers(id)
+);
