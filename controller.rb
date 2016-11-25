bd156436b323f7f51a6fe79a40983ec8d6dc3cf9
chrisbacon <bacon.chris@gmail.com>
2016-11-25 15:24:05 +0000

    added root for pet adopt page

--- controller.rb
+++ controller.rb
@@ -1,8 +1,15 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('models/pet')
+require_relative('models/customer')
 
 get '/pets' do
     @pets = Pet.all()
     erb(:index)
+end
+
+get '/pets/:id/adopt' do
+    @pet = Pet.find(params['id'])
+    @customers = Customer.all()
+    erb(:adopt)
 end
