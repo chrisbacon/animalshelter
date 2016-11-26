72b59e72de7fdd54a6c6540225b1010f7191ac8a
chrisbacon <bacon.chris@gmail.com>
2016-11-26 13:42:05 +0000

    post /pets block now sets pet adoption date to current date

--- controller.rb
+++ controller.rb
@@ -1,5 +1,6 @@
 require('sinatra')
 require('sinatra/contrib/all')
+require('date')
 require_relative('models/pet')
 require_relative('models/customer')
 
@@ -18,6 +19,7 @@ post '/pets' do
     pet = Pet.find(params['pet_id'].to_i)
     customer = Customer.find(params['customer_id'].to_i)
     pet.owner_id = customer.id
+    pet.exit = Date.today
     pet.update()
     redirect to '/pets'
 end
