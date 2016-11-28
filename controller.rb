7018235ca5ce6d97dada95569ede06ffa4d20102
chrisbacon <bacon.chris@gmail.com>
2016-11-28 11:27:15 +0000

    added delete record link to customer records on customers.erb

--- controller.rb
+++ controller.rb
@@ -38,3 +38,8 @@ get '/customers' do
     @customers = Customer.all()
     erb(:customers)
 end
+
+get '/customers/:id/delete' do
+    Customer.delete(params['id'].to_i)
+    redirect to '/customers'
+end
