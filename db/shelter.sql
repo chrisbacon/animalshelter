6905f3d25e254c5a1ad6907acea8f0ce8124d314
cbacon <bacon.chris@gmail.com>
2016-11-29 15:55:48 +0000

    added species table to shelter.sql

--- db/shelter.sql
+++ db/shelter.sql
@@ -1,4 +1,5 @@
 DROP TABLE pets;
+DROP TABLE species;
 DROP TABLE customers;
 
 CREATE TABLE customers (
@@ -6,11 +7,17 @@ CREATE TABLE customers (
     name VARCHAR(255)
 );
 
+CREATE TABLE species (
+	id SERIAL8 primary key,
+	name VARCHAR(255)
+);
+
 CREATE TABLE pets (
     id SERIAL8 primary key,
     name VARCHAR(255),
-    species VARCHAR(255),
+    species INT8 references species(id) ON DELETE CASCADE,
     entry DATE,
     exit DATE,
     owner_id INT8 references customers(id) ON DELETE SET NULL
 );
+
